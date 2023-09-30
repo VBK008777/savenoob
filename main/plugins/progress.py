f94bb4ce9a0e25eb4e83e0bb3a07cb3aa3ea07b7
VBK008777 <121817438+VBK008777@users.noreply.github.com>
2023-10-01 04:23:59 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -67,7 +67,7 @@ async def progress_for_pyrogram(
 def humanbytes(size):
     if not size:
         return ""
-    power = 2**10
+    power = 2**20
     n = 0
     Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
     while size > power:
